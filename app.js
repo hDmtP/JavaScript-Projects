3b9fa5f9b9e3d1a960a2152df35d00e27ed8622e
Pritam Dhara <65482473+hDmtP@users.noreply.github.com>
2020-12-03 11:10:54 +0530

    proj1#js

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log("Awwwwwwwwwwwwwww");
+
+// If user adsa note, add it to the local storage
+
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener('click', function (e) {
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    console.log(notes);
+})
+
+
